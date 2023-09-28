aeeebdb02eb3c7e6b7f8b354182fc32bac890551
prajithmalepati <79435415+prajithmalepati@users.noreply.github.com>
2023-09-28 09:36:26 -0400

    remove unnecessary code

--- backend/server.js
+++ backend/server.js
@@ -74,9 +74,6 @@ app.get('/employees/:idOrName', async (req, res) => {
   }
 });
 
-  // app.use((req, res, next) => {
-  //   res.status(404).json({ error: 'No routes matched location' });
-  // });
   
 app.put('/employees/:idOrName', async (req, res) => {
     try {
